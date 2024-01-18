efb2ec3d03e4ae81fe0a106983684596a939b337
bracesproul <braceasproul@gmail.com>
2024-01-18 12:40:02 -0800

    Update examples & tests

--- langgraph/src/tests/prebuilt.int.test.ts
+++ langgraph/src/tests/prebuilt.int.test.ts
@@ -4,6 +4,7 @@ import { it, beforeAll, describe, expect } from "@jest/globals";
 import { Tool } from "@langchain/core/tools";
 import { ChatOpenAI } from "@langchain/openai";
 import { BaseMessage, HumanMessage } from "@langchain/core/messages";
+import { END } from "../index.js";
 import { createFunctionCallingExecutor } from "../prebuilt/index.js";
 
 // If you have LangSmith set then it slows down the tests
@@ -52,5 +53,53 @@ describe("createFunctionCallingExecutor", () => {
     );
     expect(firstFunctionMessage).toBeDefined();
     expect(firstFunctionMessage?.content).toBe(weatherResponse);
+  });
+
+  it("can stream a function", async () => {
+    const weatherResponse = `Not too cold, not too hot 😎`;
+    const model = new ChatOpenAI({
+      streaming: true,
+    });
+    class SanFranciscoWeatherTool extends Tool {
+      name = "current_weather";
+
+      description = "Get the current weather report for San Francisco, CA";
+
+      constructor() {
+        super();
+      }
+
+      async _call(_: string): Promise<string> {
+        return weatherResponse;
+      }
+    }
+    const tools = [new SanFranciscoWeatherTool()];
+
+    const functionsAgentExecutor = createFunctionCallingExecutor<ChatOpenAI>({
+      model,
+      tools,
+    });
+
+    const stream = await functionsAgentExecutor.stream({
+      messages: [new HumanMessage("What's the weather like in SF?")],
+    });
+    const fullResponse = [];
+    for await (const item of stream) {
+      console.log(item);
+      console.log("-----\n");
+      fullResponse.push(item);
+    }
+
+    // Needs at least 3 llm calls, plus one `__end__` call.
+    expect(fullResponse.length >= 4).toBe(true);
+
+    const endMessage = fullResponse[fullResponse.length - 1];
+    expect(END in endMessage).toBe(true);
+    expect(endMessage[END].messages.length > 0).toBe(true);
+
+    const functionCall = endMessage[END].messages.find(
+      (message: BaseMessage) => message._getType() === "function"
+    );
+    expect(functionCall.content).toBe(weatherResponse);
   });
 });
