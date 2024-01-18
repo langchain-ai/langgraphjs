37595d1a7786743b698abc8b2a3868b2fc8db473
bracesproul <braceasproul@gmail.com>
2024-01-17 17:20:11 -0800

    cr

--- langgraph/src/pregel/index.ts
+++ langgraph/src/pregel/index.ts
@@ -51,7 +51,6 @@ function isString(value: unknown): value is string {
   return typeof value === "string";
 }
 
-/** @TODO make subscribe to args named after channels */
 export class Channel {
   static subscribeTo(
     channels: string,
