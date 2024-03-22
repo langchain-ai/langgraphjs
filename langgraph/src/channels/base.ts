65a6885080966c729dfe82c13d3ae6c65f1ef82f
Janvi Kalra <janvi@coda.io>
2024-03-22 09:20:54 -0700

    added some formatting

--- langgraph/src/channels/base.ts
+++ langgraph/src/channels/base.ts
@@ -12,7 +12,7 @@ export abstract class BaseChannel<
 
   /**
    * Return a new identical channel, optionally initialized from a checkpoint.
-   * Can be thought of as a "restoration" from a checkpoint which is a "snapshot" of the channel's state.  
+   * Can be thought of as a "restoration" from a checkpoint which is a "snapshot" of the channel's state.
    *
    * @param {C | undefined} checkpoint
    * @param {C | undefined} initialValue
