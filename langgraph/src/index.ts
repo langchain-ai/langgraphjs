2563b9a88697c9ea7ef0c16eed5041646d898d2b
Nuno Campos <nuno@langchain.dev>
2024-05-16 14:54:03 -0700

    Expose checkpoints, remove direct dep on sqlite

--- langgraph/src/index.ts
+++ langgraph/src/index.ts
@@ -6,12 +6,14 @@ export {
   StateGraph,
   MessageGraph,
 } from "./graph/index.js";
-export { MemorySaver } from "./checkpoint/index.js";
+export { MemorySaver } from "./checkpoint/memory.js";
 export {
   type Checkpoint,
+  type CheckpointMetadata,
+  copyCheckpoint,
   emptyCheckpoint,
   BaseCheckpointSaver,
-} from "./checkpoint/index.js";
+} from "./checkpoint/base.js";
 export {
   GraphRecursionError,
   GraphValueError,
