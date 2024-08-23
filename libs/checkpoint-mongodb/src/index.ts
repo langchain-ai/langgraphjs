b5b82fbe610445ed02522a7eadc59efbbfa74800
jacoblee93 <jacoblee93@gmail.com>
2024-08-22 17:56:35 -0700

    Adds MongoDB checkpointer

---
+++ libs/checkpoint-mongodb/src/index.ts
@@ -0,0 +1,283 @@
+import { type MongoClient, type Db as MongoDatabase } from "mongodb";
+import type { RunnableConfig } from "@langchain/core/runnables";
+import {
+  BaseCheckpointSaver,
+  type Checkpoint,
+  type CheckpointListOptions,
+  type CheckpointTuple,
+  type SerializerProtocol,
+  type PendingWrite,
+  type CheckpointMetadata,
+  CheckpointPendingWrite,
+} from "@langchain/langgraph-checkpoint";
+
+export type MongoDBSaverParams = {
+  client: MongoClient;
+  dbName?: string;
+  checkpointCollectionName?: string;
+  checkpointWritesCollectionName?: string;
+};
+
+/**
+ * A LangGraph checkpoint saver backed by a MongoDB database.
+ */
+export class MongoDBSaver extends BaseCheckpointSaver {
+  protected client: MongoClient;
+
+  protected db: MongoDatabase;
+
+  checkpointCollectionName = "checkpoints";
+
+  checkpointWritesCollectionName = "checkpoint_writes";
+
+  constructor(
+    {
+      client,
+      dbName,
+      checkpointCollectionName,
+      checkpointWritesCollectionName,
+    }: MongoDBSaverParams,
+    serde?: SerializerProtocol<Checkpoint>
+  ) {
+    super(serde);
+    this.client = client;
+    this.db = this.client.db(dbName);
+    this.checkpointCollectionName =
+      checkpointCollectionName ?? this.checkpointCollectionName;
+    this.checkpointWritesCollectionName =
+      checkpointWritesCollectionName ?? this.checkpointWritesCollectionName;
+  }
+
+  /**
+   * Retrieves a checkpoint from the MongoDB database based on the
+   * provided config. If the config contains a "checkpoint_id" key, the checkpoint with
+   * the matching thread ID and checkpoint ID is retrieved. Otherwise, the latest checkpoint
+   * for the given thread ID is retrieved.
+   */
+  async getTuple(config: RunnableConfig): Promise<CheckpointTuple | undefined> {
+    const {
+      thread_id,
+      checkpoint_ns = "",
+      checkpoint_id,
+    } = config.configurable ?? {};
+    let query;
+    if (checkpoint_id) {
+      query = {
+        thread_id,
+        checkpoint_ns,
+        checkpoint_id,
+      };
+    } else {
+      query = { thread_id, checkpoint_ns };
+    }
+    const result = await this.db
+      .collection(this.checkpointCollectionName)
+      .find(query)
+      .sort("checkpoint_id", -1)
+      .limit(1)
+      .toArray();
+    if (result.length === 0) {
+      return undefined;
+    }
+    const doc = result[0];
+    const configurableValues = {
+      thread_id,
+      checkpoint_ns,
+      checkpoint_id: doc.checkpoint_id,
+    };
+    const checkpoint = (await this.serde.parse(doc.checkpoint)) as Checkpoint;
+    const serializedWrites = await this.db
+      .collection(this.checkpointWritesCollectionName)
+      .find(configurableValues)
+      .toArray();
+    const pendingWrites: CheckpointPendingWrite[] = await Promise.all(
+      serializedWrites.map(async (serializedWrite) => {
+        return [
+          serializedWrite.task_id,
+          serializedWrite.channel,
+          await this.serde.parse(serializedWrite.value),
+        ] as const;
+      })
+    );
+    return {
+      config: { configurable: configurableValues },
+      checkpoint,
+      pendingWrites,
+      metadata: (await this.serde.parse(doc.metadata)) as CheckpointMetadata,
+      parentConfig:
+        doc.parent_checkpoint_id != undefined
+          ? {
+              configurable: {
+                thread_id,
+                checkpoint_ns,
+                checkpoint_id: doc.parent_checkpoint_id,
+              },
+            }
+          : undefined,
+    };
+  }
+
+  /**
+   * Retrieve a list of checkpoint tuples from the MongoDB database based
+   * on the provided config. The checkpoints are ordered by checkpoint ID
+   * in descending order (newest first).
+   */
+  async *list(
+    config: RunnableConfig,
+    options?: CheckpointListOptions
+  ): AsyncGenerator<CheckpointTuple> {
+    const { limit, before, filter } = options ?? {};
+    let query: Record<string, any> = {};
+
+    if (config?.configurable) {
+      query = {
+        thread_id: config.configurable.thread_id,
+        checkpoint_ns: config.configurable.checkpoint_ns ?? "",
+      };
+    }
+
+    if (filter) {
+      Object.entries(filter).forEach(([key, value]) => {
+        query[`metadata.${key}`] = value;
+      });
+    }
+
+    if (before) {
+      query.checkpoint_id = { $lt: before.configurable?.checkpoint_id };
+    }
+
+    let result = this.db
+      .collection(this.checkpointCollectionName)
+      .find(query)
+      .sort("checkpoint_id", -1);
+
+    if (limit !== undefined) {
+      result = result.limit(limit);
+    }
+
+    for await (const doc of result) {
+      const checkpoint = (await this.serde.parse(doc.checkpoint)) as Checkpoint;
+      const metadata = (await this.serde.parse(
+        doc.metadata
+      )) as CheckpointMetadata;
+
+      yield {
+        config: {
+          configurable: {
+            thread_id: doc.thread_id,
+            checkpoint_ns: doc.checkpoint_ns,
+            checkpoint_id: doc.checkpoint_id,
+          },
+        },
+        checkpoint,
+        metadata,
+        parentConfig: doc.parent_checkpoint_id
+          ? {
+              configurable: {
+                thread_id: doc.thread_id,
+                checkpoint_ns: doc.checkpoint_ns,
+                checkpoint_id: doc.parent_checkpoint_id,
+              },
+            }
+          : undefined,
+      };
+    }
+  }
+
+  /**
+   * Saves a checkpoint to the MongoDB database. The checkpoint is associated
+   * with the provided config and its parent config (if any).
+   */
+  async put(
+    config: RunnableConfig,
+    checkpoint: Checkpoint,
+    metadata: CheckpointMetadata
+  ): Promise<RunnableConfig> {
+    const thread_id = config.configurable?.thread_id;
+    const checkpoint_ns = config.configurable?.checkpoint_ns ?? "";
+    const checkpoint_id = checkpoint.id;
+    if (thread_id === undefined) {
+      throw new Error(
+        `The provided config must contain a configurable field with a "thread_id" field.`
+      );
+    }
+    const serializedCheckpoint = this.serde.stringify(checkpoint);
+    const doc = {
+      parent_checkpoint_id: config.configurable?.checkpoint_id,
+      type: "json",
+      checkpoint: serializedCheckpoint,
+      metadata: this.serde.stringify(metadata),
+    };
+    const upsertQuery = {
+      thread_id,
+      checkpoint_ns,
+      checkpoint_id,
+    };
+    this.db.collection(this.checkpointCollectionName).updateOne(
+      upsertQuery,
+      {
+        $set: doc,
+      },
+      { upsert: true }
+    );
+    return {
+      configurable: {
+        thread_id,
+        checkpoint_ns,
+        checkpoint_id,
+      },
+    };
+  }
+
+  /**
+   * Saves intermediate writes associated with a checkpoint to the MongoDB database.
+   */
+  async putWrites(
+    config: RunnableConfig,
+    writes: PendingWrite[],
+    taskId: string
+  ): Promise<void> {
+    const thread_id = config.configurable?.thread_id;
+    const checkpoint_ns = config.configurable?.checkpoint_ns;
+    const checkpoint_id = config.configurable?.checkpoint_id;
+    if (
+      thread_id === undefined ||
+      checkpoint_ns === undefined ||
+      checkpoint_id === undefined
+    ) {
+      throw new Error(
+        `The provided config must contain a configurable field with "thread_id", "checkpoint_ns" and "checkpoint_id" fields.`
+      );
+    }
+
+    const operations = writes.map(([channel, value], idx) => {
+      const upsertQuery = {
+        thread_id,
+        checkpoint_ns,
+        checkpoint_id,
+        task_id: taskId,
+        idx,
+      };
+
+      const serializedValue = this.serde.stringify(value);
+
+      return {
+        updateOne: {
+          filter: upsertQuery,
+          update: {
+            $set: {
+              channel,
+              type: "json",
+              value: serializedValue,
+            },
+          },
+          upsert: true,
+        },
+      };
+    });
+
+    await this.db
+      .collection(this.checkpointWritesCollectionName)
+      .bulkWrite(operations);
+  }
+}
---
+++ libs/checkpoint-mongodb/src/tests/checkpoints.int.test.ts
@@ -0,0 +1,133 @@
+import { describe, it, expect, afterAll } from "@jest/globals";
+import {
+  Checkpoint,
+  CheckpointTuple,
+  uuid6,
+} from "@langchain/langgraph-checkpoint";
+import { MongoDBSaver } from "../index.js";
+import { MongoClient } from "mongodb";
+
+const checkpoint1: Checkpoint = {
+  v: 1,
+  id: uuid6(-1),
+  ts: "2024-04-19T17:19:07.952Z",
+  channel_values: {
+    someKey1: "someValue1",
+  },
+  channel_versions: {
+    someKey2: 1,
+  },
+  versions_seen: {
+    someKey3: {
+      someKey4: 1,
+    },
+  },
+  pending_sends: [],
+};
+const checkpoint2: Checkpoint = {
+  v: 1,
+  id: uuid6(1),
+  ts: "2024-04-20T17:19:07.952Z",
+  channel_values: {
+    someKey1: "someValue2",
+  },
+  channel_versions: {
+    someKey2: 2,
+  },
+  versions_seen: {
+    someKey3: {
+      someKey4: 2,
+    },
+  },
+  pending_sends: [],
+};
+
+const client = new MongoClient(process.env.MONGODB_URL!);
+
+afterAll(async () => {
+  const db = client.db();
+  await db.dropCollection("checkpoints");
+  await db.dropCollection("checkpoint_writes");
+  await client.close();
+});
+
+describe("MongoDBSaver", () => {
+  it("should save and retrieve checkpoints correctly", async () => {
+    const saver = new MongoDBSaver({
+      client,
+    });
+
+    // get undefined checkpoint
+    const undefinedCheckpoint = await saver.getTuple({
+      configurable: { thread_id: "1" },
+    });
+    expect(undefinedCheckpoint).toBeUndefined();
+
+    // save first checkpoint
+    const runnableConfig = await saver.put(
+      { configurable: { thread_id: "1" } },
+      checkpoint1,
+      { source: "update", step: -1, writes: null }
+    );
+    expect(runnableConfig).toEqual({
+      configurable: {
+        thread_id: "1",
+        checkpoint_ns: "",
+        checkpoint_id: checkpoint1.id,
+      },
+    });
+
+    // get first checkpoint tuple
+    const firstCheckpointTuple = await saver.getTuple({
+      configurable: { thread_id: "1" },
+    });
+    expect(firstCheckpointTuple?.config).toEqual({
+      configurable: {
+        thread_id: "1",
+        checkpoint_ns: "",
+        checkpoint_id: checkpoint1.id,
+      },
+    });
+    expect(firstCheckpointTuple?.checkpoint).toEqual(checkpoint1);
+    expect(firstCheckpointTuple?.parentConfig).toBeUndefined();
+
+    // save second checkpoint
+    await saver.put(
+      {
+        configurable: {
+          thread_id: "1",
+          checkpoint_id: "2024-04-18T17:19:07.952Z",
+        },
+      },
+      checkpoint2,
+      { source: "update", step: -1, writes: null }
+    );
+
+    // verify that parentTs is set and retrieved correctly for second checkpoint
+    const secondCheckpointTuple = await saver.getTuple({
+      configurable: { thread_id: "1" },
+    });
+    expect(secondCheckpointTuple?.parentConfig).toEqual({
+      configurable: {
+        thread_id: "1",
+        checkpoint_ns: "",
+        checkpoint_id: "2024-04-18T17:19:07.952Z",
+      },
+    });
+
+    // list checkpoints
+    const checkpointTupleGenerator = saver.list({
+      configurable: { thread_id: "1" },
+    });
+    const checkpointTuples: CheckpointTuple[] = [];
+    for await (const checkpoint of checkpointTupleGenerator) {
+      checkpointTuples.push(checkpoint);
+    }
+    expect(checkpointTuples.length).toBe(2);
+
+    const checkpointTuple1 = checkpointTuples[0];
+    const checkpointTuple2 = checkpointTuples[1];
+    expect(checkpointTuple1.checkpoint.ts).toBe("2024-04-20T17:19:07.952Z");
+    expect(checkpointTuple2.checkpoint.ts).toBe("2024-04-19T17:19:07.952Z");
+  });
+});
