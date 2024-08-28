532597d9583a7715b63dbf4d72e9f1f8e27e2c1f
jacoblee93 <jacoblee93@gmail.com>
2024-08-28 10:59:14 -0700

    Add test, revert prebuilt

--- libs/langgraph/src/prebuilt/react_agent_executor.ts
+++ libs/langgraph/src/prebuilt/react_agent_executor.ts
@@ -21,8 +21,9 @@ import {
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { BaseCheckpointSaver } from "@langchain/langgraph-checkpoint";
 import {
+  END,
   messagesStateReducer,
-  type START,
+  START,
   StateGraph,
 } from "../graph/index.js";
 import { MessagesAnnotation } from "../graph/messages_annotation.js";
@@ -109,9 +110,9 @@ export function createReactAgent(
       isAIMessage(lastMessage) &&
       (!lastMessage.tool_calls || lastMessage.tool_calls.length === 0)
     ) {
-      return "__end__";
+      return END;
     } else {
-      return "tools";
+      return "continue";
     }
   };
 
@@ -129,8 +130,11 @@ export function createReactAgent(
       RunnableLambda.from(callModel).withConfig({ runName: "agent" })
     )
     .addNode("tools", new ToolNode<AgentState>(toolClasses))
-    .addEdge("__start__", "agent")
-    .addConditionalEdges("agent", shouldContinue, ["tools", "__end__"])
+    .addEdge(START, "agent")
+    .addConditionalEdges("agent", shouldContinue, {
+      continue: "tools",
+      [END]: END,
+    })
     .addEdge("tools", "agent");
 
   return workflow.compile({
