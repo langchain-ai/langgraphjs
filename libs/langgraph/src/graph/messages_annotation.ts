e2416c9da018d6e21f93be379ef57de963a64c2b
Jacob Lee <jacoblee93@gmail.com>
2024-09-02 23:41:32 -0700

    docs: Update more syntax in docs (#438)

--- libs/langgraph/src/graph/messages_annotation.ts
+++ libs/langgraph/src/graph/messages_annotation.ts
@@ -8,6 +8,36 @@ import { Messages, messagesStateReducer } from "./message.js";
  * Prebuilt state annotation that combines returned messages.
  * Can handle standard messages and special modifiers like {@link RemoveMessage}
  * instances.
+ *
+ * Specifically, importing and using the prebuilt MessagesAnnotation like this:
+ *
+ * @example
+ * ```ts
+ * import { MessagesAnnotation, StateGraph } from "@langchain/langgraph";
+ *
+ * const graph = new StateGraph(MessagesAnnotation)
+ *   .addNode(...)
+ *   ...
+ * ```
+ *
+ * Is equivalent to initializing your state manually like this:
+ *
+ * @example
+ * ```ts
+ * import { BaseMessage } from "@langchain/core/messages";
+ * import { Annotation, StateGraph, messagesStateReducer } from "@langchain/langgraph";
+ *
+ * export const StateAnnotation = Annotation.Root({
+ *   messages: Annotation<BaseMessage[]>({
+ *     reducer: messagesStateReducer,
+ *     default: () => [],
+ *   }),
+ * });
+ *
+ * const graph = new StateGraph(StateAnnotation)
+ *   .addNode(...)
+ *   ...
+ * ```
  */
 export const MessagesAnnotation = Annotation.Root({
   messages: Annotation<BaseMessage[], Messages>({
