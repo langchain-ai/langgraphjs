deea1c5e31301a525d6f65eac08cc0b69384b51d
bracesproul <braceasproul@gmail.com>
2024-09-11 11:17:16 -0700

    langgraph[minor]: Stores

---
+++ libs/langgraph/src/store/base.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export type Values = Record<string, any>;
+
+export abstract class BaseStore {
+  abstract list(
+    prefixes: string[]
+  ): Promise<Record<string, Record<string, Values>>>;
+
+  abstract put(writes: Array<[string, string, Values | null]>): Promise<void>;
+}
