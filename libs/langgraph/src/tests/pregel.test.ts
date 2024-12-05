5f324c186067a23680fdad684f67bd3d9608dd69
Brace Sproul <braceasproul@gmail.com>
2024-12-05 15:03:13 -0800

    fix(langgraph): Prevent non RunnableConfig keys from being passed (#707)

--- libs/langgraph/src/tests/pregel.test.ts
+++ libs/langgraph/src/tests/pregel.test.ts
@@ -8910,6 +8910,55 @@ export function runPregelTests(
     expect(result.messages).toBeDefined();
     expect(result.messages).toHaveLength(2);
   });
+
+  it("Can have three graphs with different keys", async () => {
+    const annotationOne = Annotation.Root({
+      inputOne: Annotation<string>,
+    });
+    const annotationTwo = Annotation.Root({
+      inputOne: Annotation<string>,
+      inputTwo: Annotation<string>,
+    });
+    const annotationThree = Annotation.Root({
+      inputTwo: Annotation<string>,
+      inputThree: Annotation<string>,
+    });
+
+    const graphThree = new StateGraph(annotationThree)
+      .addNode("returns", () => ({ inputThree: "one" }))
+      .addEdge(START, "returns")
+      .compile();
+
+    const graphTwo = new StateGraph(annotationTwo)
+      .addNode("one", () => ({ inputTwo: "one" }))
+      .addNode("callGraphThree", graphThree, { input: annotationThree })
+      .addEdge(START, "one")
+      .addEdge("one", "callGraphThree")
+      .addEdge("callGraphThree", END)
+      .compile();
+
+    const graphOne = new StateGraph(annotationOne)
+      .addNode("one", () => ({ inputOne: "one" }))
+      .addNode("callGraphTwo", graphTwo, { input: annotationTwo })
+      .addEdge(START, "one")
+      .addEdge("one", "callGraphTwo")
+      .addEdge("callGraphTwo", END)
+      .compile();
+
+    await expect(graphOne.invoke({ inputOne: "one" })).resolves.toBeDefined();
+  });
+
+  it("Can access store inside a node", async () => {
+    const graph = new StateGraph(MessagesAnnotation)
+      .addNode("one", async (_, configTop) => {
+        expect(configTop.store).toBeDefined();
+        return {};
+      })
+      .addEdge(START, "one")
+      .compile({ store: new InMemoryStore() });
+
+    await graph.invoke({ messages: [] });
+  });
 }
 
 runPregelTests(() => new MemorySaverAssertImmutable());
