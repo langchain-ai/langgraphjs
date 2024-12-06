38919503d8a93e8b1fac65ad960f4bdc836a30da
Jared Hanson <jaredhanson@gmail.com>
2024-12-05 16:02:40 -0800

    Allow value of NodeInterrupt to be any (JSON-serializable) type.

--- libs/langgraph/src/errors.ts
+++ libs/langgraph/src/errors.ts
@@ -62,7 +62,7 @@ export class GraphInterrupt extends GraphBubbleUp {
 
 /** Raised by a node to interrupt execution. */
 export class NodeInterrupt extends GraphInterrupt {
-  constructor(message: string, fields?: BaseLangGraphErrorFields) {
+  constructor(message: any, fields?: BaseLangGraphErrorFields) {
     super(
       [
         {
