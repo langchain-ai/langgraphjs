253f285c44f43531d81f2ae3544ccc50c851a7a7
Brace Sproul <braceasproul@gmail.com>
2024-12-05 17:06:38 -0800

    fix: Allow updating state as node end (#712)

--- libs/langgraph/src/tests/pregel.test.ts
+++ libs/langgraph/src/tests/pregel.test.ts
@@ -8959,6 +8959,29 @@ export function runPregelTests(
 
     await graph.invoke({ messages: [] });
   });
+
+  it("can interrupt then update state with asNode of __end__", async () => {
+    const graph = new StateGraph(MessagesAnnotation)
+      .addNode("one", () => {
+        console.log("Before interrupt");
+        throw new NodeInterrupt("<INTERRUPTED>");
+      })
+      .addEdge(START, "one")
+      .compile({ checkpointer: await createCheckpointer() });
+
+    const config = {
+      configurable: { thread_id: "test_update_state_as_node_end" },
+    };
+    await expect(graph.invoke({ messages: [] }, config)).resolves.toBeDefined();
+
+    const stateAfterInterrupt = await graph.getState(config);
+    expect(stateAfterInterrupt.next).toEqual(["one"]);
+
+    const updateStateResult = await graph.updateState(config, null, END);
+    expect(updateStateResult).toBeDefined();
+    const stateAfterUpdate = await graph.getState(config);
+    expect(stateAfterUpdate.next).toEqual([]);
+  });
 }
 
 runPregelTests(() => new MemorySaverAssertImmutable());
