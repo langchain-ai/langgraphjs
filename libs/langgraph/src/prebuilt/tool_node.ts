fec0af2bca4b3ae0f97489c060748bed25361e68
Jacob Lee <jacoblee93@gmail.com>
2024-12-08 18:30:37 -0800

    Update tool_node.ts

--- libs/langgraph/src/prebuilt/tool_node.ts
+++ libs/langgraph/src/prebuilt/tool_node.ts
@@ -9,7 +9,7 @@ import { StructuredToolInterface } from "@langchain/core/tools";
 import { RunnableCallable } from "../utils.js";
 import { END } from "../graph/graph.js";
 import { MessagesAnnotation } from "../graph/messages_annotation.js";
-import { NodeInterrupt } from "../errors.js";
+import { isGraphInterrupt } from "../errors.js";
 
 export type ToolNodeOptions = {
   name?: string;
@@ -188,10 +188,10 @@ export class ToolNode<T = any> extends RunnableCallable<T, T> {
           if (!this.handleToolErrors) {
             throw e;
           }
-          if (e.name === NodeInterrupt.unminifiable_name) {
+          if (isGraphInterrupt(e.name)) {
             // `NodeInterrupt` errors are a breakpoint to bring a human into the loop.
             // As such, they are not recoverable by the agent and shouldn't be fed
-            // back.  Instead, re-throw these errors even when `handleToolErrors = true`.
+            // back. Instead, re-throw these errors even when `handleToolErrors = true`.
             throw e;
           }
           return new ToolMessage({
