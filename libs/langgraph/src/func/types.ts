a17e4de025f44f0364feb3156968364b3fac5bba
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2025-01-29 17:27:54 +1300

    fix task/entrypoint func type

--- libs/langgraph/src/func/types.ts
+++ libs/langgraph/src/func/types.ts
@@ -61,12 +61,38 @@ export type EntrypointFinalValueT<OutputT> = OutputT extends
   : OutputT;
 
 /**
+ * Checks if an AsyncGenerator exists in the ES target/lib that we're compiling to.
+ *
+ * This is necessary because `tsc --init` targets ES2016 by default, which doesn't include AsyncGenerators.
+ *
+ * This works because when `skipLibCheck` is true (and it is in the default `tsconfig.json` created by `tsc --init`),
+ * TypeScript will replace any unresolved libary types with `any`. So, when `AsyncGenerator` doesn't exist, this checks
+ * if `any` extends `object`, which it doesn't. When that happens, this type resolves to the `false` literal, and we can
+ * use it in the type predicates below to skip over the AsyncGenerator-specific logic.
+ *
+ * If we didn't have this, then the types below would be checking if the user's function extends `any` in place of the
+ * `AsyncGenerator` type, and the type predicate would branch to `never`, disallowing any valid function from being passed
+ * to `task` or `entrypoint`.
+ */
+type AsyncGeneratorExists = AsyncGenerator<
+  unknown,
+  unknown,
+  unknown
+> extends object
+  ? true
+  : false;
+
+/**
  * Matches valid function signatures for entrypoints. Disallows generator functions.
  */
 export type EntrypointFunc<InputT, OutputT> = [OutputT] extends never
   ? (input: InputT, config: LangGraphRunnableConfig) => never
-  : OutputT extends AsyncGenerator<unknown, unknown, unknown>
-  ? never
+  : AsyncGeneratorExists extends true // only check if it may be an AsyncGenerator when those actually exist
+  ? OutputT extends AsyncGenerator<unknown, unknown, unknown>
+    ? never
+    : OutputT extends Generator<unknown, unknown, unknown>
+    ? never
+    : (input: InputT, config: LangGraphRunnableConfig) => OutputT
   : OutputT extends Generator<unknown, unknown, unknown>
   ? never
   : (input: InputT, config: LangGraphRunnableConfig) => OutputT;
@@ -78,8 +104,12 @@ export type TaskFunc<ArgsT extends unknown[], OutputT> = [OutputT] extends [
   never
 ]
   ? (...args: ArgsT) => never
-  : OutputT extends AsyncGenerator<unknown, unknown, unknown>
-  ? never
+  : AsyncGeneratorExists extends true // only check if it may be an AsyncGenerator when those actually exist
+  ? OutputT extends AsyncGenerator<unknown, unknown, unknown>
+    ? never
+    : OutputT extends Generator<unknown, unknown, unknown>
+    ? never
+    : (...args: ArgsT) => OutputT
   : OutputT extends Generator<unknown, unknown, unknown>
   ? never
   : (...args: ArgsT) => OutputT;
