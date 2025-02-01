5c1a89e0466a94cdffa57d01430279d4e6d1dea5
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2025-02-01 17:29:11 -0500

    updated sql queries to accept a custom schema value

--- libs/checkpoint-postgres/src/sql.ts
+++ libs/checkpoint-postgres/src/sql.ts
@@ -1,66 +1,76 @@
 import { TASKS } from "@langchain/langgraph-checkpoint";
 
-export const SELECT_SQL = `
-select
-    thread_id,
-    checkpoint,
-    checkpoint_ns,
-    checkpoint_id,
-    parent_checkpoint_id,
-    metadata,
-    (
-        select array_agg(array[bl.channel::bytea, bl.type::bytea, bl.blob])
-        from jsonb_each_text(checkpoint -> 'channel_versions')
-        inner join checkpoint_blobs bl
-            on bl.thread_id = checkpoints.thread_id
-            and bl.checkpoint_ns = checkpoints.checkpoint_ns
-            and bl.channel = jsonb_each_text.key
-            and bl.version = jsonb_each_text.value
-    ) as channel_values,
-    (
-        select
-        array_agg(array[cw.task_id::text::bytea, cw.channel::bytea, cw.type::bytea, cw.blob] order by cw.task_id, cw.idx)
-        from checkpoint_writes cw
-        where cw.thread_id = checkpoints.thread_id
-            and cw.checkpoint_ns = checkpoints.checkpoint_ns
-            and cw.checkpoint_id = checkpoints.checkpoint_id
-    ) as pending_writes,
-    (
-        select array_agg(array[cw.type::bytea, cw.blob] order by cw.idx)
-        from checkpoint_writes cw
-        where cw.thread_id = checkpoints.thread_id
-            and cw.checkpoint_ns = checkpoints.checkpoint_ns
-            and cw.checkpoint_id = checkpoints.parent_checkpoint_id
-            and cw.channel = '${TASKS}'
-    ) as pending_sends
-from checkpoints `;
+export interface SQL_STATEMENTS {
+  SELECT_SQL: string;
+  UPSERT_CHECKPOINT_BLOBS_SQL: string;
+  UPSERT_CHECKPOINTS_SQL: string;
+  UPSERT_CHECKPOINT_WRITES_SQL: string;
+  INSERT_CHECKPOINT_WRITES_SQL: string;
+}
 
-export const UPSERT_CHECKPOINT_BLOBS_SQL = `
-    INSERT INTO checkpoint_blobs (thread_id, checkpoint_ns, channel, version, type, blob)
-    VALUES ($1, $2, $3, $4, $5, $6)
-    ON CONFLICT (thread_id, checkpoint_ns, channel, version) DO NOTHING
-`;
+export const getSQLStatements = (schema: string): SQL_STATEMENTS => ({
+  SELECT_SQL: 
+`select
+  thread_id,
+  checkpoint,
+  checkpoint_ns,
+  checkpoint_id,
+  parent_checkpoint_id,
+  metadata,
+  (
+    select array_agg(array[bl.channel::bytea, bl.type::bytea, bl.blob])
+    from jsonb_each_text(checkpoint -> 'channel_versions')
+    inner join ${schema}.checkpoint_blobs bl
+      on bl.thread_id = cp.thread_id
+      and bl.checkpoint_ns = cp.checkpoint_ns
+      and bl.channel = jsonb_each_text.key
+      and bl.version = jsonb_each_text.value
+  ) as channel_values,
+  (
+    select
+    array_agg(array[cw.task_id::text::bytea, cw.channel::bytea, cw.type::bytea, cw.blob] order by cw.task_id, cw.idx)
+    from ${schema}.checkpoint_writes cw
+    where cw.thread_id = cp.thread_id
+      and cw.checkpoint_ns = cp.checkpoint_ns
+      and cw.checkpoint_id = cp.checkpoint_id
+  ) as pending_writes,
+  (
+    select array_agg(array[cw.type::bytea, cw.blob] order by cw.idx)
+    from ${schema}.checkpoint_writes cw
+    where cw.thread_id = cp.thread_id
+      and cw.checkpoint_ns = cp.checkpoint_ns
+      and cw.checkpoint_id = cp.parent_checkpoint_id
+      and cw.channel = '${TASKS}'
+  ) as pending_sends
+from ${schema}.checkpoints cp `,
 
-export const UPSERT_CHECKPOINTS_SQL = `
-    INSERT INTO checkpoints (thread_id, checkpoint_ns, checkpoint_id, parent_checkpoint_id, checkpoint, metadata)
-    VALUES ($1, $2, $3, $4, $5, $6)
-    ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id)
-    DO UPDATE SET
-        checkpoint = EXCLUDED.checkpoint,
-        metadata = EXCLUDED.metadata;
-`;
+  UPSERT_CHECKPOINT_BLOBS_SQL: 
+`INSERT INTO ${schema}.checkpoint_blobs (thread_id, checkpoint_ns, channel, version, type, blob)
+VALUES ($1, $2, $3, $4, $5, $6)
+ON CONFLICT (thread_id, checkpoint_ns, channel, version) DO NOTHING
+`,
 
-export const UPSERT_CHECKPOINT_WRITES_SQL = `
-    INSERT INTO checkpoint_writes (thread_id, checkpoint_ns, checkpoint_id, task_id, idx, channel, type, blob)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id, task_id, idx) DO UPDATE SET
-        channel = EXCLUDED.channel,
-        type = EXCLUDED.type,
-        blob = EXCLUDED.blob;
-`;
+  UPSERT_CHECKPOINTS_SQL: 
+`INSERT INTO ${schema}.checkpoints (thread_id, checkpoint_ns, checkpoint_id, parent_checkpoint_id, checkpoint, metadata)
+VALUES ($1, $2, $3, $4, $5, $6)
+ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id)
+DO UPDATE SET
+  checkpoint = EXCLUDED.checkpoint,
+  metadata = EXCLUDED.metadata;
+`,
 
-export const INSERT_CHECKPOINT_WRITES_SQL = `
-    INSERT INTO checkpoint_writes (thread_id, checkpoint_ns, checkpoint_id, task_id, idx, channel, type, blob)
-    VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
-    ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id, task_id, idx) DO NOTHING
-`;
+  UPSERT_CHECKPOINT_WRITES_SQL: 
+`INSERT INTO ${schema}.checkpoint_writes (thread_id, checkpoint_ns, checkpoint_id, task_id, idx, channel, type, blob)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id, task_id, idx) DO UPDATE SET
+  channel = EXCLUDED.channel,
+  type = EXCLUDED.type,
+  blob = EXCLUDED.blob;
+`,
+
+  INSERT_CHECKPOINT_WRITES_SQL: 
+`INSERT INTO ${schema}.checkpoint_writes (thread_id, checkpoint_ns, checkpoint_id, task_id, idx, channel, type, blob)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+ON CONFLICT (thread_id, checkpoint_ns, checkpoint_id, task_id, idx) DO NOTHING
+`,
+});
