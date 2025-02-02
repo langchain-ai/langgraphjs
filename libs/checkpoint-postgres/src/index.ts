673fe35972cd4009f32a8f6754918c34b9de7f78
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2025-02-02 01:10:39 -0500

    added schema to jsdoc example

--- libs/checkpoint-postgres/src/index.ts
+++ libs/checkpoint-postgres/src/index.ts
@@ -33,7 +33,8 @@ const { Pool } = pg;
  * import { createReactAgent } from "@langchain/langgraph/prebuilt";
  *
  * const checkpointer = PostgresSaver.fromConnString(
- *   "postgresql://user:password@localhost:5432/db"
+ *   "postgresql://user:password@localhost:5432/db",
+ *   "custom_schema" // optional schema name, defaults to 'public'
  * );
  *
  * // NOTE: you need to call .setup() the first time you're using your checkpointer
