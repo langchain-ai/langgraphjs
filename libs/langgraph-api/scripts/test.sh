6cab4be65d740322f4a1364ac3f9a54f7b730709
Tat Dat Duong <david@duong.cz>
2025-02-04 20:16:48 +0100

    Separate parser tests from api tests

--- libs/langgraph-api/scripts/test.sh
+++ libs/langgraph-api/scripts/test.sh
@@ -5,4 +5,4 @@ set -x # Print commands before execution
 rm -rf tests/graphs/.langgraph_api
 npx -y concurrently -n "server,test" -k -s "command-1" \
   "tsx ./tests/utils.server.mts" \
-  "npx -y wait-port -t 3000 localhost:2024 && vitest run"
+  "npx -y wait-port -t 3000 localhost:2024 && vitest run --exclude ./tests/parser.test.mts"
