9f3e219b58a0bbf3f4847a3584d53b5d63fe1380
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2025-02-05 10:24:33 -0500

    removed unnecessary trailing comma

--- libs/checkpoint-postgres/src/index.ts
+++ libs/checkpoint-postgres/src/index.ts
@@ -45,7 +45,7 @@ const { Pool } = pg;
  *   // optional configuration object
  *   {
  *     schema: "custom_schema" // defaults to "public"
- *   },
+ *   }
  * );
  *
  * // NOTE: you need to call .setup() the first time you're using your checkpointer
