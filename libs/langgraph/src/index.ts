3a4b522f8c43b3b5fa32a29b58981ff54d95eac2
Ben Burns <803016+benjamincburns@users.noreply.github.com>
2025-03-05 14:07:56 +1300

    feat(langgraph): add getCurrentTaskInput to fetch state in tools, etc (#943)

--- libs/langgraph/src/index.ts
+++ libs/langgraph/src/index.ts
@@ -10,3 +10,4 @@ export * from "./web.js";
 export { interrupt } from "./interrupt.js";
 export { getStore, getWriter, getConfig } from "./pregel/utils/config.js";
 export { getPreviousState } from "./func/index.js";
+export { getCurrentTaskInput } from "./pregel/utils/config.js";
