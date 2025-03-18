8587b874dc1b7bfc1bddada3eefb1abaf1acaa73
bracesproul <braceasproul@gmail.com>
2025-03-17 17:26:22 -0700

    chore: lint files

--- libs/langgraph-cua/src/index.ts
+++ libs/langgraph-cua/src/index.ts
@@ -1,1 +1,11 @@
 // CUA Agent here
+
+import { Annotation, MessagesAnnotation } from "@langchain/langgraph";
+
+const GraphAnnotation = Annotation.Root({
+  /**
+   * The message list between the user & assistant. This contains
+   * messages, including the computer use calls.
+   */
+  messages: MessagesAnnotation.spec.messages,
+});
