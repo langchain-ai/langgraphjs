09c9e06da26e8574385923dc7b305967b08dfc66
bracesproul <braceasproul@gmail.com>
2025-03-25 17:31:09 -0700

    fix(cua): Readme example imports

--- libs/langgraph-cua/src/utils.ts
+++ libs/langgraph-cua/src/utils.ts
@@ -98,7 +98,7 @@ export function getToolOutputs(
 ): ResponseComputerToolCall[] | undefined {
   const toolOutputs = message.additional_kwargs?.tool_outputs
     ? message.additional_kwargs?.tool_outputs
-    : message.response_metadata.output;
+    : message.response_metadata?.output;
 
   if (!toolOutputs || !toolOutputs.length) {
     return undefined;
