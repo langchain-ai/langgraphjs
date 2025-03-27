bb4a7d50985004359f60cfced468736bafd5e1d2
Nikhil Shahi <nshahi1998@gmail.com>
2025-03-27 05:43:58 -0500

    add hyperbrowser provider for cua agent

--- libs/langgraph-cua/src/nodes/create-vm-instance.ts
+++ libs/langgraph-cua/src/nodes/create-vm-instance.ts
@@ -1,18 +1,62 @@
 import { LangGraphRunnableConfig } from "@langchain/langgraph";
+import { chromium } from "playwright-core";
 import { UbuntuInstance, BrowserInstance, WindowsInstance } from "scrapybara";
+import { SessionDetail } from "@hyperbrowser/sdk/types";
 import { CUAState, CUAUpdate, getConfigurationWithDefaults } from "../types.js";
-import { getScrapybaraClient } from "../utils.js";
+import { getHyperbrowserClient, getScrapybaraClient } from "../utils.js";
 
-export async function createVMInstance(
+async function createHyperbrowserInstance(
   state: CUAState,
   config: LangGraphRunnableConfig
 ): Promise<CUAUpdate> {
-  const { instanceId } = state;
-  if (instanceId) {
-    // Instance already exists, no need to initialize
-    return {};
+  const { hyperbrowserApiKey, sessionParams } =
+    getConfigurationWithDefaults(config);
+  let { browserState } = state;
+
+  if (!hyperbrowserApiKey) {
+    throw new Error(
+      "Hyperbrowser API key not provided. Please provide one in the configurable fields, or set it as an environment variable (HYPERBROWSER_API_KEY)"
+    );
   }
 
+  const client = getHyperbrowserClient(hyperbrowserApiKey);
+  const session: SessionDetail = await client.sessions.create(sessionParams);
+
+  if (!browserState && session.wsEndpoint) {
+    const browser = await chromium.connectOverCDP(
+      `${session.wsEndpoint}&keepAlive=true`
+    );
+    const currPage = browser.contexts()[0].pages()[0];
+    if (currPage.url() === "about:blank") {
+      await currPage.goto("https://www.google.com");
+    }
+    browserState = {
+      browser,
+      currentPage: currPage,
+    };
+  }
+
+  if (!state.streamUrl) {
+    // If the streamUrl is not yet defined in state, fetch it, then write to the custom stream
+    // so that it's made accessible to the client (or whatever is reading the stream) before any actions are taken.
+    const streamUrl = session.liveUrl;
+    return {
+      instanceId: session.id,
+      streamUrl,
+      browserState,
+    };
+  }
+
+  return {
+    instanceId: session.id,
+    browserState,
+  };
+}
+
+async function createScrapybaraInstance(
+  state: CUAState,
+  config: LangGraphRunnableConfig
+): Promise<CUAUpdate> {
   const { scrapybaraApiKey, timeoutHours, environment, blockedDomains } =
     getConfigurationWithDefaults(config);
   if (!scrapybaraApiKey) {
@@ -55,4 +99,23 @@ export async function createVMInstance(
   return {
     instanceId: instance.id,
   };
+}
+
+export async function createVMInstance(
+  state: CUAState,
+  config: LangGraphRunnableConfig
+): Promise<CUAUpdate> {
+  const { instanceId } = state;
+  if (instanceId) {
+    // Instance already exists, no need to initialize
+    return {};
+  }
+  const { provider } = getConfigurationWithDefaults(config);
+  if (provider === "scrapybara") {
+    return createScrapybaraInstance(state, config);
+  } else if (provider === "hyperbrowser") {
+    return createHyperbrowserInstance(state, config);
+  } else {
+    throw new Error(`Unsupported provider: ${provider}`);
+  }
 }
