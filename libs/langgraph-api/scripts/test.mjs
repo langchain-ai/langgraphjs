05d56aaf22f8c6fe9c53367faeb3f558ab528beb
Tat Dat Duong <david@duong.cz>
2025-03-31 18:44:18 +0200

    Double the wait port timeout...

--- libs/langgraph-api/scripts/test.mjs
+++ libs/langgraph-api/scripts/test.mjs
@@ -11,7 +11,7 @@ await $`rm -rf tests/graphs/.langgraph_api`;
 await Promise.race([
   $`pnpm tsx ./tests/utils.server.mts`,
   (async () => {
-    await $`bun x wait-port -t 12000 localhost:2024`;
+    await $`bun x wait-port -t 24000 localhost:2024`;
     await $`pnpm vitest run --exclude ./tests/parser.test.mts"`;
     process.exit(0);
   })(),
