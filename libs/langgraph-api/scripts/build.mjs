60fce0e9c60fd21b6a0d9f36e62ce83641c4ad82
Tat Dat Duong <david@duong.cz>
2025-05-22 13:23:38 +0200

    Fix build

--- libs/langgraph-api/scripts/build.mjs
+++ libs/langgraph-api/scripts/build.mjs
@@ -2,7 +2,7 @@
 import { $ } from "./utils.mjs";
 
 await $`rm -rf dist`;
-await $`pnpm tsc --outDir dist`;
+await $`yarn tsc --outDir dist`;
 
 await $`cp src/graph/parser/schema/types.template.mts dist/src/graph/parser/schema`;
 await $`rm -rf dist/src/graph/parser/schema/types.template.mjs`;
