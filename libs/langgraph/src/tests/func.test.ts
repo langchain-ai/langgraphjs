55ab60adda742f5aac432cae0d6422bf03a884cb
Tat Dat Duong <david@duong.cz>
2025-06-04 15:22:14 +0200

    feat(langgraph): add support for multiple resumes
    Fixes missing parallel interrupts in `invoke/stream`

--- libs/langgraph/src/tests/func.test.ts
+++ libs/langgraph/src/tests/func.test.ts
@@ -945,6 +945,7 @@ export function runFuncTests(
 
       it("handles multiple interrupts from tasks", async () => {
         const addParticipant = task("add-participant", async (name: string) => {
+          console.log("addParticipant", name);
           const feedback = interrupt(`Hey do you want to add ${name}?`);
 
           if (feedback === false) {
@@ -972,7 +973,10 @@ export function runFuncTests(
 
         const config = { configurable: { thread_id } };
 
+        console.log("program.invoke");
         let result = await program.invoke([], config);
+        console.log("program.invoke.finish");
+        console.dir(result, { depth: null });
         expect(result).toEqual({
           __interrupt__: [
             {
