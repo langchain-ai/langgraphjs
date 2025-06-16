33f25a364be401a30a194c41bf7513f3361feaef
Arjun Natarajan <arjun@langchain.dev>
2025-06-16 16:11:18 -0400

    lint

--- libs/langgraph/src/pregel/index.ts
+++ libs/langgraph/src/pregel/index.ts
@@ -1809,7 +1809,10 @@ export class Pregel<
       ...options,
       // Similar to `stream`, we need to pass the `config.callbacks` here,
       // otherwise the user-provided callback will get lost in `ensureLangGraphConfig`.
-      callbacks: [...(this.config?.callbacks ?? []), ...(options?.callbacks ?? [])],
+      callbacks: [
+        ...(this.config?.callbacks ?? []),
+        ...(options?.callbacks ?? []),
+      ],
       signal: options?.signal
         ? combineAbortSignals(options.signal, abortController.signal)
         : abortController.signal,
