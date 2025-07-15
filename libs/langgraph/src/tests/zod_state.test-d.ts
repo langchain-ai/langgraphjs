ab700d77bdae32fb1b308bcc36456d3d4f0f7598
Tat Dat Duong <david@duong.cz>
2025-07-15 16:38:45 +0200

    Add prebuilt

--- libs/langgraph/src/tests/zod_state.test-d.ts
+++ libs/langgraph/src/tests/zod_state.test-d.ts
@@ -6,10 +6,38 @@ import { z as z4 } from "zod/v4";
 import { z as z4mini } from "zod/v4-mini";
 
 import { type UpdateType, withLangGraph } from "../graph/zod/meta.js";
-import { MessagesZodMeta } from "../graph/messages_annotation.js";
+import {
+  MessagesZodMeta,
+  MessagesZodState,
+} from "../graph/messages_annotation.js";
 import type { Messages } from "../graph/message.js";
 import { StateGraph } from "../graph/state.js";
 import { START } from "../constants.js";
+import { registry } from "../graph/zod/zod-registry.js";
+
+describe("zod 3 (prebuilt)", () => {
+  const state = MessagesZodState;
+
+  it("update type", () => {
+    // We're checking for correct union types here, thus we flip the assertion
+    expectTypeOf<{ messages?: Messages | undefined }>().toExtend<
+      UpdateType<typeof state>
+    >();
+  });
+
+  it("state graph", async () => {
+    const builder = new StateGraph(state);
+
+    const node = (state: {
+      messages: BaseMessage[];
+    }): { messages?: Messages } => state;
+
+    // Check for assignability
+    const graph = builder.addNode({ node }).addEdge(START, "node").compile();
+    await graph.invoke({ messages: "input" });
+    await graph.invoke({ messages: [new HumanMessage("input")] });
+  });
+});
 
 describe("zod 3", () => {
   const state = z3.object({
@@ -39,7 +67,7 @@ describe("zod 3", () => {
 
 describe("zod 4", () => {
   const state = z4.object({
-    messages: withLangGraph(z4.custom<BaseMessage[]>(), MessagesZodMeta),
+    messages: z4.custom<BaseMessage[]>().register(registry, MessagesZodMeta),
   });
 
   it("update type", () => {
@@ -65,7 +93,9 @@ describe("zod 4", () => {
 
 describe("zod 4 mini", () => {
   const state = z4mini.object({
-    messages: withLangGraph(z4mini.custom<BaseMessage[]>(), MessagesZodMeta),
+    messages: z4mini
+      .custom<BaseMessage[]>()
+      .register(registry, MessagesZodMeta),
   });
 
   it("update type", () => {
