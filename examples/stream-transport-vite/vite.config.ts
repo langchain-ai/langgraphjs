724f750e2793dcbb830f57f1fd86c875f1e2024a
Tat Dat Duong <david@duong.cz>
2025-09-16 00:34:26 +0200

    Rename to stream-transport-vite

--- examples/stream-transport-vite/vite.config.ts
+++ examples/stream-transport-vite/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), react()],
+  plugins: [react()],
   clearScreen: false,
   server: { proxy: { "/api": "http://localhost:9123" } },
 });
