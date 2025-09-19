b65c80b401b657aa7003420272a9eaaf5bfae448
David Duong <david@duong.cz>
2025-09-19 02:11:45 +0200

    feat(sdk): useStream with custom transport  (#1675)

--- libs/sdk/src/react/index.ts
+++ libs/sdk/src/react/index.ts
@@ -1,2 +1,10 @@
 export { useStream } from "./stream.js";
-export type { MessageMetadata, UseStream, UseStreamOptions } from "./types.js";
+export { FetchStreamTransport } from "./stream.custom.js";
+export type {
+  MessageMetadata,
+  UseStream,
+  UseStreamOptions,
+  UseStreamCustom,
+  UseStreamCustomOptions,
+  UseStreamTransport,
+} from "./types.js";
