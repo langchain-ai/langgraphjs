1c226ba69bc899d2aaf8f9d53eada9fa15cb0b0c
David Duong <david@duong.cz>
2025-09-20 15:23:07 +0200

    chore: fix JSDoc warnings coming from Typedoc (#1682)

--- libs/langgraph-cua/src/nodes/call-model.ts
+++ libs/langgraph-cua/src/nodes/call-model.ts
@@ -131,6 +131,7 @@ export async function callModel(
       previous_response_id: previousResponseId,
     });
 
+  /** @inline */
   type AIMessageChunkOpenAI = AIMessageChunk & {
     additional_kwargs: {
       tool_outputs?: {
