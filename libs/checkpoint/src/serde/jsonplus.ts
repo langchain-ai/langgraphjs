99e41b4169bd775531d6ecabcd2977b1fc126b64
John Kennedy <65985482+jkennedyvz@users.noreply.github.com>
2026-02-07 18:49:43 -0800

    bump eslint

--- libs/checkpoint/src/serde/jsonplus.ts
+++ libs/checkpoint/src/serde/jsonplus.ts
@@ -82,7 +82,7 @@ async function _reviver(value: any): Promise<any> {
 }
 
 function _encodeConstructorArgs(
-  // eslint-disable-next-line @typescript-eslint/ban-types
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-function-type
   constructor: Function,
   method?: string,
   args?: any[],
