fdaef5816347b9995dbea4c25590f838cd3512e6
Hunter Lovell <hunter@hntrl.io>
2026-04-13 22:42:58 -0700

    fix(langgraph): import CallbackManager from callbacks manager

--- libs/langgraph-core/src/pregel/index.ts
+++ libs/langgraph-core/src/pregel/index.ts
@@ -1,6 +1,6 @@
 /* eslint-disable no-param-reassign */
+import { CallbackManager } from "@langchain/core/callbacks/manager";
 import {
-  CallbackManager,
   _coerceToRunnable,
   mergeConfigs,
   patchConfig,
