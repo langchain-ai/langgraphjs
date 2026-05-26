8235f8e2227cd24e2f972342740d2b6ee4c414a0
Christian Bromann <git@bromann.dev>
2026-05-26 16:51:15 -0700

    cr

--- libs/sdk/src/react/useStream.test-d.ts
+++ libs/sdk/src/react/useStream.test-d.ts
@@ -728,7 +728,7 @@ describe("useStream.values state inference", () => {
 
     const stream = useStream<DirectState>({
       transport,
-      onFinish() {},
+      onFinish() { },
     });
 
     expectTypeOf(stream.values).toEqualTypeOf<DirectState>();
@@ -886,8 +886,6 @@ describe("@langchain/langgraph-sdk/react backward compatibility", () => {
     expectTypeOf(stream.isLoading).toEqualTypeOf<boolean>();
     expectTypeOf(stream.error).toEqualTypeOf<unknown>();
     expectTypeOf(stream.stop()).toEqualTypeOf<Promise<void>>();
-    expectTypeOf(stream.stop({ cancel: false })).toEqualTypeOf<Promise<void>>();
-    expectTypeOf(stream.disconnect()).toEqualTypeOf<Promise<void>>();
     expectTypeOf(stream.branch).toEqualTypeOf<string>();
   });
 
